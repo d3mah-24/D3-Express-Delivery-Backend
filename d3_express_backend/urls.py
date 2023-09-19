c39658485e5ad6474cd1af9ed738324a310ab365
d3mah-24 <ahmedshitech2011@gmail.com>
2023-09-19 08:19:37 +0300

    Add Fetch Orders

--- d3_express_backend/urls.py
+++ d3_express_backend/urls.py
@@ -10,6 +10,7 @@ from accounts.views import login_check
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/<str:username>/<str:password>", login_check),
-    path("order/", OrderCreatelistView.as_view()),
+    path("orders/<str:address>", OrderCreatelistView.as_view()),
+    path("order", OrderCreatelistView.as_view()),
     path("order/<str:waybillno>", OrderRetriveUpdateView.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -1,12 +1,19 @@
 from rest_framework import generics
 from order.models import Orders
 from order.serializer import OrderSerializer
+from django.db.models import Q
 
 
 class OrderCreatelistView(generics.ListCreateAPIView):
-    queryset = Orders.objects.all()
     lookup_field = "waybillno"
     serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        address = self.kwargs.get("address")
+        queryset = Orders.objects.filter(
+            Q(receiverAddress=address) | Q(receiverAddress=address)
+        )
+        return queryset
 
 
 class OrderRetriveUpdateView(generics.RetrieveUpdateDestroyAPIView):
