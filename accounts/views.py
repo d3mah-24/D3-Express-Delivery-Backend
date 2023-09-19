73d70b77ff14b9d1f416ef093760d2a32205bc60
d3mah-24 <ahmedshitech2011@gmail.com>
2023-09-19 06:52:57 +0300

    Add Return Address

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
 
 User = get_user_model()
 
@@ -8,13 +8,16 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
-def login_check(req,username,password):
+def login_check(req, username, password):
     if req.method == "GET":
-        print(req.POST) 
+        print(req.POST)
         a = authenticate(username=username, password=password)
+
         print(username, password, a)
         if a is not None:
-            return HttpResponse(
+            login(req, a)
+            
+            return HttpResponse(a.address,
                 status=200,
             )
-    return HttpResponse(status=404)
+    return HttpResponse( status=404)
