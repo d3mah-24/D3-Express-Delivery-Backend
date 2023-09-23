88b447419976aebaa70737abca7da996f8e4bdd2
d3mah-24 <ahmedshitech2011@gmail.com>
2023-09-23 14:03:36 +0300

    Fix CSRF

--- d3_express_backend/settings.py
+++ d3_express_backend/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "d3_express_backend.wsgi.application"
- 
+
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 # Database
